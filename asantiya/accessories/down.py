d782f8b054e14d907b16b5469b81bff78dc1a1f7
Shahid Khan <shahid.khannn66@gmail.com>
2025-06-21 17:27:06 +0500

    [REFACTOR] remove unnecessary code

--- asantiya/accessories/down.py
+++ asantiya/accessories/down.py
@@ -1,8 +1,6 @@
 import typer
 from typing import Annotated, List
-from pathlib import Path
 from asantiya.docker_manager import DockerManager
-from asantiya.utils.config import load_config
 from asantiya.logger import setup_logging
 from asantiya.utils.docker import setup_connection
 
@@ -16,14 +14,13 @@ def down(
         accessories: Annotated[List[str], typer.Option(help="Single or list of accessories name")] = None,
         volumes: Annotated[bool, typer.Option("--volumes", "-v", help='Remove named volumes declared in the "volumes" section of the Yaml file and anonymous volumes attached to containers', is_flag=True, show_default="False")] = False
     ) -> None:
-    config = load_config(Path().cwd() / "deploy.yaml")
     docker_manager = DockerManager()
 
     try:
-        setup_connection(config, docker_manager)
+        setup_connection(docker_manager)
 
         if not accessories:
-            accessories = docker_manager.list_accessory_services(config.accessories) 
+            accessories = docker_manager.list_accessory_services(docker_manager.config.accessories) 
         docker_manager.stop_accessories(accessories, volumes)
 
     except Exception as e:
