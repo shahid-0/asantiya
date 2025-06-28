dae85449d9e55988b14a93ee640ff63a563f96ad
Shahid Khan <shahid.khannn66@gmail.com>
2025-06-28 17:26:56 +0500

    [FEAT] create accessories when deploy app

--- asantiya/cli.py
+++ asantiya/cli.py
@@ -2,6 +2,7 @@ from typing import Optional
 import typer
 
 from asantiya.accessories import app as accessories_app
+from asantiya.app import app as asantiya_app
 from asantiya.deploy import app as deploy_app
 from asantiya.init import app as init_app
 
@@ -9,6 +10,7 @@ from asantiya import __app_name__, __version__
 
 app = typer.Typer(no_args_is_help=True)
 app.add_typer(accessories_app, name="accessory")
+app.add_typer(asantiya_app, name="app")
 app.add_typer(deploy_app)
 app.add_typer(init_app)
 
